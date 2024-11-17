b3bec540008c06e1fdcaa5399deea8ee07ddef30
Francisco Martinez <franciscomj345@gmail.com>
2024-11-17 12:16:50 -0700

    better error handling

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,31 @@
 
 import { API_KEY } from '@env';
+import { BookDetails } from '../../App';
 
-
-export const fetchBookDetails = async (isbn: string) => {
+export const fetchBookDetails = async (isbn: string): Promise<BookDetails> => {
+  try {
     const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&key=${API_KEY}`);
-    return await response.json();
+
+    if (!response.ok) {
+      const errorMessage = `Error: ${response.status} ${response.statusText}`;
+      throw new Error(errorMessage);
+    }
+
+    const data = await response.json();
+
+    if (!data?.items) {
+      throw new Error(`Invalid response: ${JSON.stringify(data)}`);
+    }
+
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error('An unknown error occurred');
+    }
+    throw error;
+  }
+
   };
   
